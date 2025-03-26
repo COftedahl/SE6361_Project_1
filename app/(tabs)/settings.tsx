c4e1f8db1e58e8fa41df86caebd1ceeccb606aa0
Cole <oftedahl.cole@gmail.com>
2025-03-25 21:50:37 -0500

    Populated Settings Screen

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,32 @@
 import RangeInput from "@/components/FigmaRendering/RangeInput";
+import Select from "@/components/FigmaRendering/Select";
+import MicrophoneSVGIcon from "@/components/FigmaRendering/svg/Microphone";
+import RabbitSVGIcon from "@/components/FigmaRendering/svg/Rabbit";
+import TurtleSVGIcon from "@/components/FigmaRendering/svg/Turtle";
 import TabLayoutManager from "@/components/FigmaRendering/TabComponents/TabLayoutManager";
 import { ThemedText } from "@/components/FigmaRendering/ThemedText";
 import { ThemedView } from "@/components/FigmaRendering/ThemedView";
 import { Colors } from "@/constants/Colors";
+import { BaseFontSize } from "@/constants/Font";
+import { standardBorderRadius } from "@/constants/Styles";
+import { useState } from "react";
+import { StyleSheet, TextInput } from 'react-native';
+import ToggleSwitch from "toggle-switch-react-native";
 
+
+const inputWidth = "60%";
+const inputPadding = 15;
 
 const SettingsTab = () => {
+  
+  const SVGICON_DIMENSIONS: number = 50;
+
+  const [alertEmergencyContactFirst, setAlertEmergencyContactFirst] = useState<boolean>(false);
+
+  const handleEmergencyContactSliderChanged = (newVal: boolean) => {
+    setAlertEmergencyContactFirst(newVal);
+  }
+
   return (
     <TabLayoutManager pageTitle={""} headerOptions={{
       displayStrideLengthButton: true,
@@ -13,24 +34,240 @@ const SettingsTab = () => {
       displayReadScreenButton: true
     }}>
       <ThemedView>
-        <ThemedText>
-          abcd
+        {/* Start section Display */}
+        <ThemedText style={styles.sectionTitleText}>
+          Display
         </ThemedText>
-        <RangeInput 
-          min={0} 
-          max={10} 
-          step={1} 
-          thumbShadowRadius={5} 
-          thumbShadowColor={Colors.black} 
-          inactiveRailShadowRadius={4} 
-          inactiveRailColor={Colors.lightgrey}
-          inactiveRailShadowColor={Colors.lightgrey} 
-          inactiveRailShadowOffset={{width: 0, height: 2}}
-          disableRange
-        />
+        {/* Start subsection Text Size */}
+        <ThemedText style={styles.subsectionTitleText}>
+          Text Size
+        </ThemedText>
+        <ThemedView style={styles.sliderSettingRowDiv}>
+          <ThemedText style={{fontSize: 1 * BaseFontSize}}>
+            A
+          </ThemedText>
+          <ThemedView style={styles.sliderDiv}>
+            <RangeInput 
+              min={0} 
+              max={10} 
+              step={1} 
+              thumbShadowRadius={5} 
+              thumbShadowColor={Colors.black} 
+              inactiveRailShadowRadius={4} 
+              inactiveRailColor={Colors.lightgrey}
+              inactiveRailShadowColor={Colors.lightgrey} 
+              inactiveRailShadowOffset={{width: 0, height: 2}}
+              disableRange
+            />
+          </ThemedView>
+          <ThemedText style={{fontSize: 2 * BaseFontSize}}>
+            A
+          </ThemedText>
+        </ThemedView>
+
+        <hr style={styles.divider}/>
+
+        {/* Start section Audio */}
+        <ThemedText style={styles.sectionTitleText}>
+          Audio
+        </ThemedText>
+        {/* Start subsection Instruction Speed */}
+        <ThemedText style={styles.subsectionTitleText}>
+          Instruction Speed
+        </ThemedText>
+        <ThemedView style={styles.sliderSettingRowDiv}>
+          <TurtleSVGIcon strokeWidth={"2"} strokeColor={Colors.white} backgroundColor={Colors.black} width={SVGICON_DIMENSIONS} height={SVGICON_DIMENSIONS}/>
+          <ThemedView style={styles.sliderDiv}>
+            <RangeInput 
+              min={0} 
+              max={10} 
+              step={1} 
+              thumbShadowRadius={5} 
+              thumbShadowColor={Colors.black} 
+              inactiveRailShadowRadius={4} 
+              inactiveRailColor={Colors.lightgrey}
+              inactiveRailShadowColor={Colors.lightgrey} 
+              inactiveRailShadowOffset={{width: 0, height: 2}}
+              disableRange
+            />
+          </ThemedView>
+          <RabbitSVGIcon strokeWidth={"2"} strokeColor={Colors.white} backgroundColor={Colors.black} width={SVGICON_DIMENSIONS} height={SVGICON_DIMENSIONS}/>
+        </ThemedView>
+        {/* Start subsection Instruction Interval */}
+        <ThemedText style={styles.subsectionTitleText}>
+          Instruction Speed
+        </ThemedText>
+        <ThemedView style={styles.sliderSettingRowDiv}>
+          <ThemedView style={styles.centeredColumnFlex}>
+            <ThemedText>
+              2
+            </ThemedText>
+            <ThemedText>
+              seconds
+            </ThemedText>
+          </ThemedView>
+          <ThemedView style={styles.sliderDiv}>
+            <RangeInput 
+              min={2} 
+              max={15} 
+              step={1} 
+              thumbShadowRadius={5} 
+              thumbShadowColor={Colors.black} 
+              inactiveRailShadowRadius={4} 
+              inactiveRailColor={Colors.lightgrey}
+              inactiveRailShadowColor={Colors.lightgrey} 
+              inactiveRailShadowOffset={{width: 0, height: 2}}
+              disableRange
+            />
+          </ThemedView>
+          <ThemedView style={styles.centeredColumnFlex}>
+            <ThemedText>
+              15
+            </ThemedText>
+            <ThemedText>
+              seconds
+            </ThemedText>
+          </ThemedView>
+        </ThemedView>
+
+        <hr style={styles.divider}/>
+
+        {/* Start section Emergency */}
+        <ThemedText style={styles.sectionTitleText}>
+          Emergency
+        </ThemedText>
+        {/* Start subsection Emergency Contact */}
+        <ThemedText style={styles.subsectionTitleText}>
+          Emergency Contact
+        </ThemedText>
+        {/* Start input row "Name" */}
+        <ThemedView style={styles.inputRowDiv}>
+          <ThemedText style={styles.textInputLabel}>
+            Name
+          </ThemedText>
+          <TextInput placeholder={"Enter Name Here"} style={styles.textInput}/>
+          <MicrophoneSVGIcon height={40} width={40} strokeWidth={"5"}/>
+        </ThemedView>
+        {/* Start input row "Phone Number" */}
+        <ThemedView style={styles.inputRowDiv}>
+          <ThemedText style={styles.textInputLabel}>
+            Phone Number
+          </ThemedText>
+          <TextInput placeholder={"Enter Name Here"} style={styles.textInput}/>
+          <MicrophoneSVGIcon height={40} width={40} strokeWidth={"5"}/>
+        </ThemedView>
+        {/* Start input row "Address" */}
+        <ThemedView style={styles.inputRowDiv}>
+          <ThemedText style={styles.textInputLabel}>
+            Address
+          </ThemedText>
+          <TextInput placeholder={"Enter Name Here"} style={styles.textInput}/>
+          <MicrophoneSVGIcon height={40} width={40} strokeWidth={"5"}/>
+        </ThemedView>
+        {/* Start input row "Dial Emergency Contact Instead of 911" */}
+        <ThemedView style={styles.toggleRowDiv}>
+          <ThemedText style={{}}>
+            Dial Emergency Contact Instead of 911
+          </ThemedText>
+          <ToggleSwitch
+            isOn={alertEmergencyContactFirst} 
+            onToggle={handleEmergencyContactSliderChanged}
+            onColor={Colors.blue}
+            size="large"
+          />
+        </ThemedView>
+        
+        <hr style={styles.divider}/>
+
+        {/* Start section Language */}
+        <ThemedText style={styles.sectionTitleText}>
+          Language
+        </ThemedText>
+
+        <ThemedView style={styles.toggleRowDiv}>
+          <ThemedText style={styles.textInputLabel}>
+            Language
+          </ThemedText>
+          <Select 
+            placeholder={{label: "Select a Language", value: "", color: Colors.lightgrey}}
+            onValueChange={() => {}} 
+            items={
+              [
+                {label: "English", value: "English"},
+                {label: "Spanish", value: "Spanish"},
+                {label: "French", value: "French"},
+                {label: "German", value: "German"}
+              ]
+            }
+            borderRadius={2 * standardBorderRadius}
+            borderColor={Colors.lightgrey}
+            width={inputWidth}
+            padding={inputPadding}
+          />
+          <MicrophoneSVGIcon height={40} width={40} strokeWidth={"5"}/>
+        </ThemedView>
+        
+
       </ThemedView>
     </TabLayoutManager>
   )
 }
+
+const styles = StyleSheet.create({
+  sectionTitleText: {
+    fontWeight: 500, 
+    fontSize: 1.4 * BaseFontSize, 
+    paddingVertical: 5,
+  },
+  subsectionTitleText: {
+    fontWeight: 500, 
+    paddingBottom: 0,
+    paddingLeft: 10,
+  },
+  sliderSettingRowDiv: {
+    flexDirection: "row", 
+    justifyContent: "space-between", 
+    paddingHorizontal: 10,
+    marginBottom: 15,
+  }, 
+  sliderDiv: {
+    flexGrow: 1,
+    paddingHorizontal: 10, 
+    paddingTop: 10,
+  }, 
+  divider: {
+    width: "90%", 
+    marginTop: 15,
+  }, 
+  centeredColumnFlex: {
+    flexDirection: "column", 
+    alignItems: "center", 
+  }, 
+  inputRowDiv: {
+    flexDirection: "row", 
+    justifyContent: "flex-start",
+    paddingHorizontal: 20, 
+    alignItems: "center", 
+    marginVertical: 3,
+  },
+  textInputLabel: {
+    width: "30%", 
+  }, 
+  textInput: {
+    borderRadius: 2 * standardBorderRadius, 
+    borderWidth: 1, 
+    borderColor: Colors.lightgrey, 
+    padding: inputPadding, 
+    paddingVertical: 12,
+    width: inputWidth, 
+  }, 
+  toggleRowDiv: {
+    flexDirection: "row", 
+    justifyContent: "space-between", 
+    paddingHorizontal: 20, 
+    alignItems: "center", 
+    marginVertical: 10,
+  }, 
+})
 
 export default SettingsTab;
--- components/FigmaRendering/Select.tsx
+++ components/FigmaRendering/Select.tsx
@@ -12,28 +12,41 @@ interface Props extends PickerSelectProps{
   padding?: DimensionValue | number, 
   width?: DimensionValue | number, 
   height?: DimensionValue | number,
+  borderRadius?: DimensionValue | number,
+  borderColor?: string, 
+  borderWidth?: DimensionValue | number,
 }
 
 const Select = (props: Props) => {
 
+  const selectStylesObj = {
+    padding: (props.padding ? props.padding : 10),
+    width: "100%",
+    borderColor: (props.borderColor ? props.borderColor : Colors.lightgrey), 
+    borderWidth: (props.borderWidth ? props.borderWidth : 1), 
+    borderRadius: (props.borderRadius ? props.borderRadius : standardBorderRadius), 
+  };
+
   return (
     <ThemedView 
-      style={
+      style={[
         {
           position: "relative", 
           width: (props.width ? props.width : "fit-content") as any, 
           height: (props.height ? props.height : "auto") as any, 
           margin: (props.margin ? props.margin : 0),
-        }
-      }
+        }, 
+      ]}
     >
       <Picker 
         {...props}
         Icon={() => <DownArrow strokeColor={Colors.grey} strokeWidthNumber={0}/>}
         style={
           {
-            ...styles.selectStyles, 
-            padding: (props.padding ? props.padding : 0),
+            inputIOS: {...selectStylesObj, ...styles.selectStyles},
+            inputAndroid: {...selectStylesObj, ...styles.selectStyles},
+            inputWeb: {...selectStylesObj, ...styles.selectStyles},
+            iconContainer: {...styles.iconContainer, top: ((selectStylesObj.padding as number )/ 2 + 2)},
           }
         }
       >
@@ -44,20 +57,20 @@ const Select = (props: Props) => {
 
 const styles=StyleSheet.create({
   selectStyles: {
-    inputIOS: {
+    // inputIOS: {
 
-    }, 
-    inputAndroid: {
+    // }, 
+    // inputAndroid: {
 
-    }, 
-    inputWeb: {
+    // }, 
+    // inputWeb: {
       alignSelf: "flex-start", 
-      width: "auto",
-      padding: 10,
-      borderColor: Colors.lightgrey, 
-      borderWidth: 1, 
+      // width: "auto",
+      // padding: 10,
+      // borderColor: Colors.lightgrey, 
+      // borderWidth: 1, 
       borderStyle: "solid", 
-      borderRadius: standardBorderRadius, 
+      // borderRadius: standardBorderRadius, 
       backgroundColor: Colors.transparent,
       zIndex: 1,
       // position: "relative", 
@@ -66,15 +79,15 @@ const styles=StyleSheet.create({
       WebkitAppearance: "none",
       appearance: "none",
       paddingRight: 50,
-    }, 
+    } as any, 
     iconContainer: {
       right: 0,
-      top: 7,
+      // top: 7,
       zIndex: 0,
       // backgroundColor: Colors.white,
       // color: Colors.white,
     }
-  } as any,
+  // } as any,
 })
 
 export default Select;
